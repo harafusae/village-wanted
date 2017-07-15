db1836c3ca6d5ff999459a55a6f5c1b5ca889366
FusaeHara <suyamafusae@gmail.com>
2017-07-16 03:41:10 +0900

    途中で一回コミット

---
+++ js/common.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+
+//FirstView
+
+
+
+//Sec_Photos
+  $('.photo_slider').bxSlider({
+    //mode: 'fade',
+    auto: true,
+    speed: 200,
+    pause: 3000,
+    pager: true,
+    controls: false,
+    captions: true,
+    responsive: true,
+    touchEnabled: false,
+    oneToOneTouch: false,
+    useCSS: false
+  });
+
+});
